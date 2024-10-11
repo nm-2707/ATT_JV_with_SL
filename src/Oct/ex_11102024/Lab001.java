9795155b3c0021c2da42f799ea3d18ebbbba6c94
nitishmani <nitishmani64@gmail.com>
2024-10-11 13:11:02 +0530

    second program

---
+++ src/Oct/ex_11102024/Lab001.java
@@ -0,0 +1,7 @@
+package Oct.ex_11102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
---
+++ src/Oct/ex_11102024/Lab002.java
@@ -0,0 +1,8 @@
+package Oct.ex_11102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Second program");
+        System.out.println("2 X 1 =2");
+    }
+}
